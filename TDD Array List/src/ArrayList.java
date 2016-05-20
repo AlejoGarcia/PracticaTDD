ab5a9a1af42ac438a180752c39578721032e8d3e
alejo <alejogarciagalan@gmail.com>
2016-05-20 20:40:50 +0200

    El codigo ha pasado el primer test

--- TDD Array List/src/ArrayList.java
+++ TDD Array List/src/ArrayList.java
@@ -1,4 +1,28 @@
 
 public class ArrayList {
-
+	
+	Nodo inicio, fin;
+	
+	public ArrayList(){
+		inicio = null;
+		fin = null;
+	}
+	
+	public void put(String clave, String valor){
+		if(inicio == null){
+			inicio = new Nodo(clave,valor,null);
+			fin = inicio;
+		} else{
+			fin.setSiguiente(new Nodo(clave,valor,null));
+			fin = fin.getSiguiente();
+		}
+	}
+	
+	public String get(String clave){
+		Nodo aux = inicio;
+		while((aux.getClave()!=clave)&&(aux != null)){
+			aux = aux.getSiguiente();
+		}
+		return aux.getValor();
+	}
 }
